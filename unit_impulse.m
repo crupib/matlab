17f1b24e91e0ef31e24dba1f8446fdcbe1cef9de
crupib <crupib@gmail.com>
2022-12-20 22:15:23 -0500

    first matlab dsp

---
+++ unit_impulse.m
@@ -0,0 +1,16 @@
+n = 4;
+t = -n:1:n;
+y1 = [zeros(1,n),1,zeros(1,n)];
+subplot(2,2,1);
+stem(t,y1);
+ylabel('d[n]');
+xlabel('n');
+title('Unit impulse function');
+n = 4;
+t = 0:1:n;
+y2 = [ones(1,n+1)];
+subplot(2,2,2);
+stem(t,y2);
+ylabel('u[n]');
+xlabel('n');
+title('Unit step function');
