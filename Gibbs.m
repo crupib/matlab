4bd74780c396458e1bdff024d9c6247cf540ed3e
crupib <crupib@gmail.com>
2023-08-31 15:52:15 -0400

    Fourier and Gibbs

---
+++ Gibbs.m
@@ -0,0 +1,16 @@
+t = linspace(-2,2,10000);
+f = 0*t;
+N=1000;
+for n = -N:1:N
+    if(n==0)
+        continue;
+    end;
+    C_n = ((1)/(pi*1i*n))*(1-exp(-pi*1i*n));
+    f_n = C_n*exp(2*pi*1i*n*t);
+    f = f+f_n;
+end
+plot(t,f, 'LineWidth',2);
+grid on;
+xlabel('t');
+ylabel('f(t)');
+title(strcat('Fourier synthesis of the square wave function withn n=',int2str(N), ' harmonics.'));
